4e30433edc04d7d1e32f480d1c9066b75fbf613c
Emmanuel Egomson <femmor2u@gmail.com>
2020-05-13 15:31:08 -0500

    added show and hide password icon

--- src/App.js
+++ src/App.js
@@ -2,29 +2,39 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
-  const [ userName, setUsername ] = useState("")
+  const [ username, setUsername ] = useState("")
   const [ password, setPassword ] = useState("")
   const [ showPassword, setShowPassword ] = useState(false)
 
   return (
     <div className="container">
+        <h2>React Hooks Form</h2>
         <input 
           type="text"
           placeholder="Enter Username"
-          value={userName}
+          value={username}
           onChange={(e) => setUsername(e.target.value)}
-        />
-        <input 
-          type={showPassword ? "text" : "password"}
-          placeholder="Enter Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <h2>{userName}</h2>
-        <h2>{showPassword ? password : "*".repeat(password.length)}</h2>
-        <button onClick={(e) => setShowPassword(!showPassword)}>{showPassword ? "Hide Password" : "Show Password"}</button>
-    </div>
-  )
+          />
+
+         <div className="passwordInput">
+            <input 
+            type={showPassword ? "text" : "password"}
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
+            <span className="password-icon-text">{showPassword ? "hide password" : "show password"}</span>
+            <span className="password-icon" onClick={(e) => setShowPassword(!showPassword)}><i className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}></i></span>
+         </div>
+
+          <div className="info">
+              <h3>Your Username is: {username}</h3>
+              <h3>Your Password is: {
+                showPassword ? password : "*".repeat(password.length)
+              }</h3>
+          </div>
+      </div>
+    )
 }
 
 export default App;
